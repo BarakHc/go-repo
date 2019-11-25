a5215655f4351e86a0b9e5c981799aad46b2e10b
Barak Hacham <barakh@jfrog.com>
2019-11-25 16:42:16 +0200

    added some things

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/BarakHc/go-repo
+module m
 
 go 1.13
+
+require rsc.io/quote v3.1.0+incompatible
--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
+import "rsc.io/quote"
 
 func main() {
 	fmt.Printf("hello V23, world\n")
+}
+
+func Hello() string {
+    return quote.Hello()
 }
